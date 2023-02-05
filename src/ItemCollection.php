73c023b77a963d4e9f9a95801dec21cba5f1da65
Nikolay Gorohovsky <fi1a@icloud.com>
2023-02-05 13:17:09 +0500

    Возвращаемые коллекции файлов по типу контента из коллекции элементов

--- src/ItemCollection.php
+++ src/ItemCollection.php
@@ -5,12 +5,32 @@ declare(strict_types=1);
 namespace Fi1a\Crawler;
 
 use Fi1a\Collection\Collection;
+use Fi1a\Http\Mime;
+
+use const PATHINFO_EXTENSION;
 
 /**
  * Коллекция элементов
  */
 class ItemCollection extends Collection implements ItemCollectionInterface
 {
+    /**
+     * @var array<array-key, string>
+     */
+    protected static $imageMimes = [
+        'image/gif', 'image/jpeg', 'image/png', 'image/bmp', 'image/tiff',
+    ];
+
+    /**
+     * @var array<array-key, string>
+     */
+    protected static $pageMimes = [Mime::HTML, Mime::XHTML];
+
+    /**
+     * @var array<array-key, string>
+     */
+    protected static $jsMimes = ['application/javascript', 'text/javascript'];
+
     /**
      * @inheritDoc
      */
@@ -52,6 +72,77 @@ class ItemCollection extends Collection implements ItemCollectionInterface
             assert($item instanceof ItemInterface);
 
             return $item->getWriteStatus() === true;
+        });
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getImages()
+    {
+        $mimes = static::$imageMimes;
+
+        return $this->filter(function ($item) use ($mimes) {
+            assert($item instanceof ItemInterface);
+
+            return in_array($item->getContentType(), $mimes);
+        });
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getFiles()
+    {
+        $mimes = array_merge(static::$imageMimes, static::$pageMimes, static::$jsMimes);
+
+        return $this->filter(function ($item) use ($mimes) {
+            assert($item instanceof ItemInterface);
+            $extension = pathinfo($item->getItemUri()->uri(), PATHINFO_EXTENSION);
+
+            return !in_array($item->getContentType(), $mimes) && mb_strtolower($extension) !== 'css';
+        });
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getPages()
+    {
+        $mimes = static::$pageMimes;
+
+        return $this->filter(function ($item) use ($mimes) {
+            assert($item instanceof ItemInterface);
+
+            return in_array($item->getContentType(), $mimes);
+        });
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getCss()
+    {
+        return $this->filter(function ($item) {
+            assert($item instanceof ItemInterface);
+
+            $extension = pathinfo($item->getItemUri()->uri(), PATHINFO_EXTENSION);
+
+            return mb_strtolower($extension) === 'css';
+        });
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getJs()
+    {
+        $mimes = static::$jsMimes;
+
+        return $this->filter(function ($item) use ($mimes) {
+            assert($item instanceof ItemInterface);
+
+            return in_array($item->getContentType(), $mimes);
         });
     }
 }
--- tests/ItemCollectionTest.php
+++ tests/ItemCollectionTest.php
@@ -6,6 +6,7 @@ namespace Fi1a\Unit\Crawler;
 
 use Fi1a\Crawler\Item;
 use Fi1a\Crawler\ItemCollection;
+use Fi1a\Http\Mime;
 use Fi1a\Http\Uri;
 use Fi1a\Unit\Crawler\TestCases\TestCase;
 
@@ -65,5 +66,78 @@ class ItemCollectionTest extends TestCase
         $item = new Item(new Uri('/link1.html'));
         $collection[] = $item;
         $this->assertCount(1, $collection->getWrited());
+    }
+
+    /**
+     * Изображения
+     */
+    public function testImages(): void
+    {
+        $collection = new ItemCollection();
+        $item = new Item(new Uri('/image.gif'));
+        $item->setContentType('image/gif');
+        $collection[] = $item;
+        $collection[] = new Item(new Uri('/link1.html'));
+        $this->assertCount(2, $collection);
+        $this->assertCount(1, $collection->getImages());
+    }
+
+    /**
+     * Страницы
+     */
+    public function testPages(): void
+    {
+        $collection = new ItemCollection();
+        $item = new Item(new Uri('/index.html'));
+        $item->setContentType(Mime::HTML);
+        $collection[] = $item;
+        $collection[] = new Item(new Uri('/link1.html'));
+        $this->assertCount(2, $collection);
+        $this->assertCount(1, $collection->getPages());
+    }
+
+    /**
+     * CSS файлы
+     */
+    public function testCss(): void
+    {
+        $collection = new ItemCollection();
+        $item = new Item(new Uri('/style.css'));
+        $collection[] = $item;
+        $collection[] = new Item(new Uri('/link1.html'));
+        $this->assertCount(2, $collection);
+        $this->assertCount(1, $collection->getCss());
+    }
+
+    /**
+     * JS файлы
+     */
+    public function testJs(): void
+    {
+        $collection = new ItemCollection();
+        $item = new Item(new Uri('/script.js'));
+        $item->setContentType('application/javascript');
+        $collection[] = $item;
+        $collection[] = new Item(new Uri('/link1.html'));
+        $this->assertCount(2, $collection);
+        $this->assertCount(1, $collection->getJs());
+    }
+
+    /**
+     * Файлы
+     */
+    public function testFiles(): void
+    {
+        $collection = new ItemCollection();
+        $item = new Item(new Uri('/script.js'));
+        $item->setContentType('application/javascript');
+        $collection[] = $item;
+        $item = new Item(new Uri('/style.css'));
+        $collection[] = $item;
+        $item = new Item(new Uri('/file.docx'));
+        $item->setContentType('application/vnd.openxmlformats-officedocument.wordprocessingml.document');
+        $collection[] = $item;
+        $this->assertCount(3, $collection);
+        $this->assertCount(1, $collection->getFiles());
     }
 }
